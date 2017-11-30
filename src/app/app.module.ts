7475de7a5c613eb560d2336422d6baadf57706a8
Klec Max <max.klec@do-it.co>
2017-11-30 16:21:53 +0200

    First commit with router configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent }          from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const appRoutes: Routes = [
+  { path: 'first-page', component: FirstComponentComponent },
+  { path: 'second-page', component: SecondComponentComponent },
+  { path: '', component: WelcomeComponent },
+
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    NotFoundComponent,
+    FirstComponentComponent,
+    SecondComponentComponent,
+    WelcomeComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  template: `<h1>Page not found</h1>`,
+  styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
